126e31823008e577c69847131b2f912dc4c7141a
Stephen Mullins <smullins7@gmail.com>
2010-01-12 17:52:17 +0000

    wrapper can take a file to construct a config

--- wrapper.py
+++ wrapper.py
@@ -1,8 +1,14 @@
 from configuration import Config
 
 class Wrapper(object):
-    def __init__(self, config):
-        self.config = config
+    def __init__(self, config=None, file=None):
+        if config:
+            self.config = config
+        elif file:
+            self.config = Config(file)
+
+    def disabled(self):
+        self.config.disable()
 
     def output(self, line):
         print self.config.format(line)
